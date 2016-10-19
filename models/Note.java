f9b5ec4470776cbb8338f708496ec91232350252
Roomsterdsg <roomsterdsg@gmail.com>
2016-10-19 15:12:25 +0300

    EditorActivity working almost good but needs 2 times pressing BACK somettimes
    Here is the desctiption
    http://valokafor.com/create-android-notepad-app-part-1/
    http://valokafor.com/create-android-notepad-app-part-2/

---
+++ models/Note.java
@@ -0,0 +1,70 @@
+package com.okason.simplenotepad.models;
+
+import android.database.Cursor;
+
+import com.okason.simplenotepad.utilities.Constants;
+
+/**
+ * Created by Valentine on 9/28/2015.
+ */
+public class Note {
+    private Long id;
+    private String title;
+    private String content;
+    private long dateCreated;
+    private long dateModified;
+
+
+    public static Note getNotefromCursor(Cursor cursor){
+        Note note = new Note();
+        note.setId(cursor.getLong(cursor.getColumnIndex(Constants.COLUMN_ID)));
+        note.setTitle(cursor.getString(cursor.getColumnIndex(Constants.COLUMN_TITLE)));
+        note.setContent(cursor.getString(cursor.getColumnIndex(Constants.COLUMN_CONTENT)));
+        note.setDateCreated(cursor.getLong(cursor.getColumnIndex(Constants.COLUMN_CREATED_TIME)));
+        note.setDateModified(cursor.getLong(cursor.getColumnIndex(Constants.COLUMN_MODIFIED_TIME)));
+        return note;
+    }
+
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public long getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(long dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public long getDateModified() {
+        return dateModified;
+    }
+
+    public void setDateModified(long dateModified) {
+        this.dateModified = dateModified;
+    }
+}
